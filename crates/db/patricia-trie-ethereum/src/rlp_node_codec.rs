b00ec59af5e4503ca02c8b7f45fb202b6d478e23
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2022-10-18 22:32:58 +0500

    get rid of primitive_types version

--- crates/db/patricia-trie-ethereum/src/rlp_node_codec.rs
+++ crates/db/patricia-trie-ethereum/src/rlp_node_codec.rs
@@ -87,7 +87,7 @@ impl NodeCodec<KeccakHasher> for RlpNodeCodec<KeccakHasher> {
             Some(r.as_val().expect("Hash is the correct size; qed"))
         } else {
             None
-            
+// remove me
         }
     }
     fn is_empty_node(data: &[u8]) -> bool {
